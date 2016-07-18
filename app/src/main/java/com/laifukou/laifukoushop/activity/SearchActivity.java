0333f03767a520c7d99cf57d8c70b1327989f3df
jimmy <hu.gw@ikcrm.com>
2016-07-18 15:38:36 +0800

    update bug

---
+++ app/src/main/java/com/laifukou/laifukoushop/activity/SearchActivity.java
@@ -0,0 +1,23 @@
+package com.laifukou.laifukoushop.activity;
+
+import com.laifukou.laifukoushop.R;
+import com.laifukou.laifukoushop.activity.base.BaseListActivity;
+
+public class SearchActivity extends BaseListActivity {
+
+
+    @Override
+    public void loadLoyout() {
+        setContentView(R.layout.activity_search);
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void requestDatas() {
+
+    }
+}
