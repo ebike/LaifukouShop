77014b11099643a641f36322b081798651c4d930
jimmy <hu.gw@ikcrm.com>
2016-07-20 23:33:33 +0800

    我的ui

--- app/src/main/java/com/laifukou/laifukoushop/fragment/MyFragment.java
+++ app/src/main/java/com/laifukou/laifukoushop/fragment/MyFragment.java
@@ -1,9 +1,19 @@
 package com.laifukou.laifukoushop.fragment;
 
+import android.os.Bundle;
+
+import com.laifukou.laifukoushop.R;
 import com.laifukou.laifukoushop.common.fragment.LazyFragment;
 
 /**
  * 我的
  */
 public class MyFragment extends LazyFragment {
+
+    @Override
+    protected void onCreateViewLazy(Bundle savedInstanceState) {
+        super.onCreateViewLazy(savedInstanceState);
+        setContentView(R.layout.fragment_my);
+
+    }
 }
